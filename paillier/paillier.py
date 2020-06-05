b6344f3deb0399d0978ab57755c92b4baff70423
shadowtinkery <shadowtinkery@gmail.com>
2020-06-05 15:09:44 +0800

    finish enc and dec function

--- paillier/paillier.py
+++ paillier/paillier.py
@@ -2,7 +2,7 @@
 """
 import random, sys 
 
-from gmpy2 import mpz, powmod, invert, is_prime, random_state, mpz_urandomb, rint_round, log2, gcd 
+from gmpy2 import mpz, powmod, invert, is_prime, random_state, mpz_urandomb, rint_round, log2, gcd, mpz_random, rint_floor, mul, div
 
 rand=random_state(random.randrange(sys.maxsize))
 
@@ -41,27 +41,34 @@ def generate_keypair(bits):
     n = p * q
     return PrivateKey(p, q, n), PublicKey(n)
 
-def enc(pub, plain):#(public key, plaintext) #to do
+def enc(pub, plain):#(public key, plaintext) #TODO
+    cipher = mpz()
+    r = mpz_random(rand, pub.n)
+    while (gcd(r, pub.n) != 1):
+        r = mpz_random(rand, pub.n)
+    cipher = powmod(mul(powmod(pub.g, plain, pub.n_sq), powmod(r, pub.n, pub.n_sq)), 1, pub.n_sq)
     return cipher
 
-def dec(priv, pub, cipher): #(private key, public key, cipher) #to do
+def dec(priv, pub, cipher): #(private key, public key, cipher) #TODO
+    L = (pow(cipher, priv.l, pub.n_sq) - 1) // pub.n
+    plain = (L * priv.m) % pub.n
     return plain
 
-def enc_add(pub, m1, m2): #to do
+def enc_add(pub, m1, m2): #TODO
     """Add one encrypted integer to another"""
     return 
 
-def enc_add_const(pub, m, c): #to do
+def enc_add_const(pub, m, c): #TODO
     """Add constant n to an encrypted integer"""
     return
 
-def enc_mul_const(pub, m, c): #to do
+def enc_mul_const(pub, m, c): #TODO
     """Multiplies an encrypted integer by a constant"""
     return
 
 if __name__ == '__main__':
     priv, pub = generate_keypair(1024)
-    """
-    test
-    """
+    cipher = enc(pub, mpz(1212122147))
+    plain = dec(priv, pub, cipher)
+    print(plain)
 
